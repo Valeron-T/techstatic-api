db26e5a28ead082a95d650c938e474a433c1628f
Valeron <32789691+Valeron-T@users.noreply.github.com>
2024-01-06 00:04:24 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -63,13 +63,13 @@ def hello():
 def new_qr(response_id: str):
     print(os.getcwd())
     try:
-        qr = qrcode.QRCode(box_size=14)
+        qr = qrcode.QRCode(box_size=12)
         qr_string = response_id
         img = ticket
         qr.add_data(qr_string)
         qr.make()
         img_qr = qr.make_image(fill_color="black", back_color="#E6E6FA")
-        pos = (1440, 90)
+        pos = (1200, 80)
         img.paste(img_qr, pos)
         stream = BytesIO()
         img.save(stream, format='JPEG')
